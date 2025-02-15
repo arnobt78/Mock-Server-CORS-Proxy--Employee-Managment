eff298317b95307bd5849394cf7665a2bcb11036
Arnob Mahmud <arnob_t78@yahoo.com>
2025-02-15 17:39:59 +0600

    update server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const corsAnywhere = require('cors-anywhere');
+const corsAnywhere = require("cors-anywhere");
 
-const host = '0.0.0.0';
+const host = "0.0.0.0";
 const port = 8080;
 
-corsAnywhere.createServer({
-  originWhitelist: [], // Allow all origins
-  requireHeader: ['origin', 'x-requested-with'],
-  removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, () => {
-  console.log(`CORS Anywhere server running on ${host}:${port}`);
-});
+corsAnywhere
+  .createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: [], // Do not require any headers
+    removeHeaders: ["cookie", "cookie2"],
+  })
+  .listen(port, host, () => {
+    console.log(`CORS Anywhere server running on ${host}:${port}`);
+  });
+
+// corsProxyUrl: string = 'https://cors-proxy-avym.onrender.com/';
+
+// corsProxyUrl: string = 'http://localhost:8080/';
